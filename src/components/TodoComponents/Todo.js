f040c3937b6a053f826ebb094b0009a4a2bac30e
CryptoN999 <nicholasgallucci4@gmail.com>
2019-10-31 16:51:39 -0700

    working on imports/app.js/component files

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Todo.css";
+
+
+
+
+
+function ToDo({ todo, toggleCompleted }) {
+  return (
+    <li
+      className={todo.completed ? `completed` : null}
+      onClick={() => toggleCompleted(todo.id)}
+    >
+      {todo.task}
+    </li>
+  );
+}
+
+
+export default ToDo;
